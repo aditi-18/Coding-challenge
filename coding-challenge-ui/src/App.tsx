53139021881824a3d96744aeda02b878b02dfb85
aditishri1896 <Year@last1896>
2024-06-03 10:58:30 +1000

    cleanup

--- coding-challenge-ui/src/App.tsx
+++ coding-challenge-ui/src/App.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
-// 
 import styled from "styled-components";
 import OrderTable from "./OrderTable";
-// 
 const AppWrapper = styled.div`
   height: 100vh;
   width: 100vw;
   background-color: #cccccc;
 `;
-// 
+
 const AppHeader = styled.header`
   background-color: white;
   display: flex;
@@ -16,25 +14,20 @@ const AppHeader = styled.header`
   align-items: center;
   padding: 0rem 2rem;
 `;
-// 
 const HeaderText = styled.h1`
   font-family: "Roboto", sans-serif;
 `;
-// 
 const Username = styled.span`
   font-family: "Roboto", sans-serif;
 `;
-// 
 interface User {
   firstName: string;
   lastName: string;
   email: string;
   id: number;
-}
-// 
+} 
 const App = () => {
-  const [user, setUser] = useState<User | null>(null);
-// 
+  const [user, setUser] = useState<User | null>(null); 
   React.useEffect(() => {
     fetch("http://localhost:8080/user")
       .then((results) => results.json())
@@ -42,16 +35,15 @@ const App = () => {
         setUser(data);
       });
   }, []);
-// 
+
   return (
     <AppWrapper>
       <AppHeader>
         <HeaderText>Analytics Dashboard</HeaderText>
         <Username>Welcome, {user ? user.firstName : "Guest"}!</Username>
       </AppHeader>
-      <OrderTable /> {/* Render the OrderTable component */}
+      <OrderTable />
     </AppWrapper>
   );
 };
-// 
 export default App;
